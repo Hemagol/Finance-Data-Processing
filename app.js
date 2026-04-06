ce8908798525004c708292e277d6f9583e426d26
Hemanth Kumar <hemagol117@gmail.com>
2026-04-06 17:55:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,4 +10,4 @@ app.use("/api/auth", auth_routes);
 app.use("/api/users", user_routes);
 app.use("/api/transactions", txn_routes);
 app.get("/", (req, res) => res.json({ message: "Finance Backend API running" }));
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(3000, () => console.log("Server running on port 3000")):
